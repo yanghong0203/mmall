c0e24f2c6a1bd72516267548bc6ad511dee36098
yanghong0203 <1207626126@qq.com>
2018-09-07 13:21:08 +0800

    commit 2018-9-6 13:20 session时间重置

--- src/main/java/com/mmall/controller/common/SessionExpireFilter.java
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -1,10 +1,12 @@
 package com.mmall.controller.common;
 
 import com.mmall.common.Const;
+import com.mmall.common.RedisShardedPool;
 import com.mmall.pojo.User;
 import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.*;
@@ -23,10 +25,10 @@ public class SessionExpireFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         String loginToken = CookieUtil.readLoginToken(httpServletRequest);
         if (StringUtils.isNotEmpty(loginToken)){
-            String userJsonStr = RedisPoolUtil.get(loginToken);
+            String userJsonStr = RedisShardedPoolUtil.get(loginToken);
             User user = JsonUtil.string2Obj(userJsonStr,User.class);
             if (user != null){
-                RedisPoolUtil.expire(loginToken,Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+                RedisShardedPoolUtil.expire(loginToken,Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
             }
         }
         chain.doFilter(request,response);
